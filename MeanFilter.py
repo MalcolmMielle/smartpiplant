8fa9f0fdce27c277c3c9c4174da288a9f5c28e7d
MalcolmMielle <malcolm.mielle@protonmail.com>
2016-03-17 00:27:16 +0000

    bug correction

--- MeanFilter.py
+++ MeanFilter.py
@@ -15,13 +15,18 @@ class MeanFilter(object):
 		
 	def addValue(self, val):
 		if(len(self.values) +1 > self.max):
+			#print("pop")
 			self.values.pop(0)
 		self.values.append(val)
 		
 	def getMean(self):
+		#print("getmean")
 		sum = 0
 		for elem in self.values:
+			#print("elem")
+			#print(elem)	
 			sum = sum + elem
-		print("lenght")
-		print(len(self.values))
-		return elem / len(self.values)
+		#print("lenght")
+		#print(len(self.values))
+		#print(elem)
+		return sum / len(self.values)
